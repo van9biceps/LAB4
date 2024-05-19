79e5e32ebc2e176bd965ef6df002bd3241f3d3a5
root <root@debian.debian.ivan>
2024-05-19 14:26:24 +0300

    Создана документация

---
+++ doxygen/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['recover_5fsecret_0',['recover_secret',['../namespaceexample.html#a8d57eee5e2c56338938fe6a25a23f9e1',1,'example']]]
+];
---
+++ doxygen/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "er",
+  1: "e",
+  2: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Functions"
+};
+
